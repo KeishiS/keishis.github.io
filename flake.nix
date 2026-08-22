6577cfd5b846de06f209d45f28b684838936938f
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2026-08-22 14:03:18 +0900

    updated settings

--- flake.nix
+++ flake.nix
@@ -2,29 +2,37 @@
   description = "CV environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    adocweave.url = "github:KeishiS/adocweave?ref=v0.44.1";
   };
 
   outputs =
     {
       self,
       nixpkgs,
+      adocweave,
     }:
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+      };
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          pnpm
-          nodejs_24
-          bubblewrap
+        packages =
+          with pkgs;
+          [
+            pnpm
+            nodejs_26
 
-          typst
-          typstyle
-          typst-live
-        ];
+            typst
+            typstyle
+            typst-live
+          ]
+          ++ [
+            adocweave.packages.${system}.default
+          ];
         shellHook = ''
           export SHELL="${pkgs.zsh}"/bin/zsh
           exec ${pkgs.zsh}/bin/zsh

0909ade8b0cf8e66ac8636900961da0b79661b54
KeishiS <sando.keishi.sp@alumni.tsukuba.ac.jp>
2026-08-22 13:45:12 +0900

    removed an agent command

--- flake.nix
+++ flake.nix
@@ -3,36 +3,28 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      nixpkgs-unstable,
     }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
-      pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
     in
     {
       devShells.${system}.default = pkgs.mkShell {
-        packages =
-          with pkgs;
-          [
-            pnpm
-            nodejs_24
-            bubblewrap
+        packages = with pkgs; [
+          pnpm
+          nodejs_24
+          bubblewrap
 
-            typst
-            typstyle
-            typst-live
-          ]
-          ++ [
-            pkgs-unstable.claude-code
-          ];
+          typst
+          typstyle
+          typst-live
+        ];
         shellHook = ''
           export SHELL="${pkgs.zsh}"/bin/zsh
           exec ${pkgs.zsh}/bin/zsh
